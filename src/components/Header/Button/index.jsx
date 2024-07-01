f66aa3c3b0024750c920bc45b025abc7df79a2f2
flaviofaul <flavinhochicshow@gmail.com>
2024-07-01 11:54:26 -0300

    GIT PUSH

---
+++ src/components/Header/Button/index.jsx
@@ -0,0 +1,15 @@
+
+import {MainButton} fron ".styles"
+
+function Button({ children}){
+
+      return <MainButton>{Children}</MainButton>
+      
+      }
+      
+            
+
+    
+
+  
+export default Button
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,29 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import api from '../../services/api'
-import { Background } from './styles'
+import { Background,Info, Poster,Container, ContainerButtons } from './styles'
+import Movies from '../Movies'
+import Button from '../../components/Header/Button/styles'
 
+function Home() {
+  const [movie, setMovie] = useState([])
 
-function Home(){
+  useEffect(() => {
+    async function getMovies() {
+      try {
+        const { data } = await api.get('/movie/popular')
+        setMovie(data.results[0])
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
-   async function getMovies() {
+    getMovies()
+  }, [])
 
-       const data = await api.get('/movie/popular')
-      
+  return (
+    <div>
 
-       console.log(data)
-  }
-  getMovies()
-
-         return (
-      <div>
-        <Background img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} >
-        <h1>Home</h1>
-        <p>Essa é a Home</p>
-        </Background>
-      </div>
-
-
-
-         )
+      {movie && (
+      <Background
+        img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+      >
+        <Container>
+        <Info>
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+        <ContainerButtons>
+          
+          <Button >Assista Agora</Button>
+          <Button >Assista o Trailer</Button>
+        </ContainerButtons>
+        </Info>
+        <Poster>
+        <img alt="capa-do-filme" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+        </Poster>
+        </Container>
+      </Background>
+      )}
+    </div>
+  );
 }
 export default Home
