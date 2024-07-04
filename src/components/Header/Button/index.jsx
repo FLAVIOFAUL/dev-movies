35f68957c274400dc04092cf6a143f1deafd7edb
flaviofaul <flavinhochicshow@gmail.com>
2024-07-04 09:45:35 -0300

    git push

--- src/components/Header/Button/index.jsx
+++ src/components/Header/Button/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import  { ButtonRed, ButtonWhite } from './styles';
+import { ButtonRed, ButtonWhite } from './styles';
 
 function Button({ children, red }) {
   return (
-  <>
-  {red ? (
-    <ButtonRed>{children}</ButtonRed>
-  ):(
-    <ButtonWhite>{children}</ButtonWhite>
-  )}
+    <>
+      {red ? (
+        <ButtonRed>{children}</ButtonRed>
+      ) : (
+        <ButtonWhite>{children}</ButtonWhite>
+      )}
     </>
-  )
+  );
 }
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired, 
+  children: PropTypes.node.isRequired,
+  red: PropTypes.bool, 
 };
 
 export default Button;
