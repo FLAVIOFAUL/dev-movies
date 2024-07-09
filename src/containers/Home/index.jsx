a262c3ebcff3cc8c4f40ddca1d145ac1c5ca9f53
flaviofaul <flavinhochicshow@gmail.com>
2024-07-09 17:11:42 -0300

    git push

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from 'react'
+
 import api from '../../services/api'
-import { Background,Info, Poster,Container, ContainerButtons } from './styles'
-import Movies from '../Movies'
-import Button from '../../components/Header/Button/styles'
+import { Background, Info, Poster, Container, ContainerButtons } from './styles' 
+
+import Button from '../../components/Button'
+import Slider from '../../components/Slider'
 
 function Home() {
   const [movie, setMovie] = useState([])
   const [topMovies, setMovies] = useState()
+
   useEffect(() => {
+    async function getMovies() {
+      try {
+        const { data } = await api.get('/movie/popular')
+        setMovie(data.results[0])
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
     async function getTopMovies() {
       try {
         const { data } = await api.get('/movie/top_rated')
-        setMovie(data.results[0])
+        setMovies(data.results)
         console.log(data)
       } catch (error) {
         console.log(error)
@@ -23,18 +36,18 @@ function Home() {
   }, [])
 
   return (
-    <div>
+    <>
 
       {movie && (
       <Background
-        img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-      >
+        img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}>
+
         <Container>
         <Info>
         <h1>{movie.title}</h1>
         <p>{movie.overview}</p>
         <ContainerButtons>
-          
+
           <Button red >Assista Agora</Button>
           <Button >Assista o Trailer</Button>
         </ContainerButtons>
@@ -45,7 +58,8 @@ function Home() {
         </Container>
       </Background>
       )}
-    </div>
-  );
+     {topMovies && <Slider info={topMovies} title={'Top Filmes'} /> }
+    </>
+  )
 }
 export default Home
