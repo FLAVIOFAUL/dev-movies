195a7c209a5c2d8523a70b4a098e5d9e6999fdfc
flaviofaul <flavinhochicshow@gmail.com>
2024-07-16 16:50:38 -0300

    git push

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,32 @@
 import Logo from "../../assets/logo.png"
-    
+import { Link, useLocation } from "react-router-dom"    
 
-import React from "react";
+import React from "react"
+import { Container, Menu, Li } from "./styles"
 
 
 function Header() {
-
+     const { pathname} =useLocation()
      return (
-         <div>
+         <Container>
               <h1>Header</h1>
-              <img src={Logo} alt="logo-dev-movies" style={{ width: 500 }}/>
+              <img src={Logo} alt="logo-dev-movies" />
+              <Menu>
+               <Li isActive={pathname === '/'}>
+                <Link to="/">Home</Link>
+                </Li>
 
+                <Li isActive={pathname.includes('filmes')}>
+                <Link to="/">Filmes</Link>
+                </Li>
 
-         </div>
+                <Li isActive={pathname.includes('series')}>
+                <Link to="/">Series</Link>
+                </Li>
+
+              </Menu>
+
+         </Container>
 
      )
 
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,84 +1,65 @@
-import React, { useState, useEffect } from 'react'
-import { getImages } from '../../utils/getImages'
-import api from '../../services/api'
-import { Background, Info, Poster, Container, ContainerButtons } from './styles' 
+import React, { useState, useEffect } from 'react';
+import { getImages } from '../../utils/getImages';
+import api from '../../services/api';
+import { Background, Info, Poster, Container, ContainerButtons } from './styles';
 
-import Button from '../../components/Button'
-import Slider from '../../components/Slider'
+import Button from '../../components/Button';
+import Slider from '../../components/Slider';
 
 function Home() {
-  const [movie, setMovie] = useState([])
-  const [topMovies, setMovies] = useState()
-  const [topSeries, setTopSeries]= useState()
-  
+  const [movie, setMovie] = useState([]);
+  const [topMovies, setTopMovies] = useState(); 
+  const [topSeries, setTopSeries] = useState();
+
   useEffect(() => {
     async function getMovies() {
       try {
-        const { data } = await api.get('/movie/popular')
-        setMovie(data.results[0])
-        console.log(data)
+        const { data } = await api.get('/movie/popular');
+        setMovie(data.results[0]);
+        setTopMovies(data.results); 
+        console.log(data);
       } catch (error) {
-        console.log(error) 
+        console.log(error);
       }
     }
-    useEffect(() => {
-      async function getMovies() {
-        try {
-          const { data } = await api.get('/movie/popular')
-          setMovie(data.results[0])
-          console.log(data)
-        } catch (error) {
-          console.log(error)
-        }
-      }
-      useEffect(() => {
+
     async function getTopSeries() {
       try {
-        const { data } = await api.get('/tv/top_rated')
-        setTopSeries(data.results)
-        console.log(data)
+        const { data } = await api.get('/tv/top_rated');
+        setTopSeries(data.results);
+        console.log(data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     }
-    
-    getMovies()
-    getTopMovies()
-    getTopSeries()
 
-  }, [])
+    getMovies();
+    getTopSeries();
+  }, []);
 
   return (
     <>
-
       {movie && (
-      <Background
-        img={getImages(movie.backdrop_path)}>
-
-        <Container>
-        <Info>
-        <h1>{movie.title}</h1>
-        <p>{movie.overview}</p>
-        <ContainerButtons>
-
-          <Button red >Assista Agora</Button>
-          <Button >Assista o Trailer</Button>
-        </ContainerButtons>
-        </Info>
-        <Poster>
-        <img alt="capa-do-filme" src={getImages(movie.poster_path)}/>
-        </Poster>
-        </Container>
-      </Background>
+        <Background img={getImages(movie.backdrop_path)}>
+          <Container>
+            <Info>
+              <h1>{movie.title}</h1>
+              <p>{movie.overview}</p>
+              <ContainerButtons>
+                <Button red>Assista Agora</Button>
+                <Button>Assista o Trailer</Button>
+              </ContainerButtons>
+            </Info>
+            <Poster>
+              <img alt="capa-do-filme" src={getImages(movie.poster_path)} />
+            </Poster>
+          </Container>
+        </Background>
       )}
-     {topMovies && <Slider info={topMovies} title={'Top Filmes'} /> }
-     {topSeries && <Slider info={topSeries} title={'Top Series'} /> }
+      {topMovies && <Slider info={topMovies} title={'Top Filmes'} />}
+      {topSeries && <Slider info={topSeries} title={'Top Series'} />}
     </>
-  
-  )
-} 
-  
+  );
+}
 
-
-
- export default Home 
+export default Home;
