466a571e16cec71f1f538b7969741e66b8712ca6
flaviofaul <flavinhochicshow@gmail.com>
2024-08-26 12:30:30 -0300

    git push

--- src/containers/Detail/index.jsx
+++ src/containers/Detail/index.jsx
@@ -1,7 +1,37 @@
 import { Container } from './style'
 import React from 'react'
- 
+import { useEffect } from 'react' 
+import { getMovieById, getMovieCredits, getMovieSimilar, getMovieVideos } from '../../services/getData';
+import {useParams} from 'react-router-dom'
+
 function Detail() {
+ const { id } = useParams()
+   const [movie, setMovie] = useState()
+   const [movieVideos, setMovieVideos] = useState()
+   const [movieCredits, setMovieCredits] = useState()
+   const [movieSimilar, setMovieSimilar] = useState()
+
+  useEffect(() => {
+    async function getAllData() {
+      Promise.all([
+        getMovieById(id),
+        getMovieVideos(id),
+        getMovieCredits(id),
+        getMovieSimilar(id),
+        
+      ])
+        .then(([movie, Videos, Credits, Similar ]) => {
+          setMovie(movie);
+          setMovieVideos(Videos);
+          setMovieCredits(Credits);
+          setMovieSimilar(Similar);
+          
+        })
+        .catch((error) => console.error(error));
+    }
+       getAllData()
+  },[])
+
       return (
 
     <Container>
